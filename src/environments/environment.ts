96d49046677ccb841a7e99f43be62e8c35c5b562
bryanbill <emobrya@gmail.com>
2022-04-17 20:30:28 +0300

    feat:env - github api config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Github API
+  githubApiUrl: 'https://api.github.com/',
+  githubApiToken: '',
+  
 };
 
 /*
